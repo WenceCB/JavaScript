7687c6354ded763895179938f25000e9d1c6f66c
Wence Criado Baez <wence74@gmail.com>
2017-01-17 09:15:23 +0100

    Iterar Array de forma Asíncrona con tratamiento y Callback de salida

--- pruebaAsync/async.js
+++ pruebaAsync/async.js
@@ -9,14 +9,17 @@ var async= require('async');
 var arr = ['uno','dos','tres'];
 
 // Definir un iterador
+// Con SetTimeout posponemos la siguiente vuelta del EventLoop de Node para no colapsarlo
 
 var iterador = function(elemento,callback){
-    if(elemento === 'dos') {
-        return callback(null);
-    }
-    elemento = elemento + ' -procesado';
-    callback(null, elemento);
-    return;
+    setTimeout(function(){
+        if(elemento === 'dos') {
+            return callback(null);
+        }
+        elemento = elemento + ' -procesado';
+        callback(null, elemento);
+        return;
+    },0);
 };
 
 // Procesar el array
